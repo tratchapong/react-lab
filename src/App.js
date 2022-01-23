1996adf6808408a166a695d9499b938198f28eb9
tratchapong <tratchapong@gmail.com>
2022-01-24 01:41:48 +0700

    BS5 Navbar with full import : index.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid">
+      <header className="p-3 mb-3 border-bottom">
+        <div className="container">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+              <img src="https://picsum.photos/40" />
+            </a>
+
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" className="nav-link px-2 link-secondary">Overview</a></li>
+              <li><a href="#" className="nav-link px-2 link-dark">Inventory</a></li>
+              <li><a href="#" className="nav-link px-2 link-dark">Customers</a></li>
+              <li><a href="#" className="nav-link px-2 link-dark">Products</a></li>
+            </ul>
+
+            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+              <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
+            </form>
+
+            <div className="dropdown text-end">
+              <a href="#" className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+              </a>
+              <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                <li><a className="dropdown-item" href="#">New project...</a></li>
+                <li><a className="dropdown-item" href="#">Settings</a></li>
+                <li><a className="dropdown-item" href="#">Profile</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Sign out</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
